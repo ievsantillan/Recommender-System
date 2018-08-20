7b94521953e6578f629e34adbffce33760b77898
siopow sisig <ievsantillan@gmail.com>
2018-08-20 09:40:29 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Big Data - Recommender System, Amazon',
+    author='ian santillan',
+    license='MIT',
+)
